3de4b68997974fdfd6aca1d243bdef802231ab36
Filipe Cabecinhas <github@filcab.net>
2019-10-09 22:14:42 +0100

    map.js: Remove complicated fall-back logic. Add UK Ordnance Survey vector maps

--- map.js
+++ map.js
@@ -1,46 +1,3 @@
-// FIXME: This should probably be patched in mapbox-gl-leaflet
-// For now, do it during map.js initialization to guarantee any created object
-// is fixed.
-//
-// Mapbox-GL-Leaflet will throttle a function call, which will make it
-// be called at most one time every X interval. If we have some of those updates
-// pending when we remove the layer, they'll be called later. By then, this._map
-// is undefined, so they'll crash. Monkey-patch the _update() function to check
-// this._map and return immediately if it is null.
-// FIXME: Couldn't repro the problem after adding this code, and couldn't break
-// on the replacement function. Might not be fully working.
-const old_update = L.MapboxGL.prototype._update;
-L.MapboxGL.prototype._update = function() {
-  if (this._map === undefined) {
-    console.debug(
-        'called _update after map was removed! We should send the patch to mapbox-gl-leaflet');
-    return;
-  }
-  old_update.apply(this, arguments);
-};
-
-
-// TODO: Have a key file in the server, fetch it, fetch the base JSON with that
-// key. If anything fails, we can't use MapTiler.
-async function canFetchMapTiler() {
-  // This url is embedded in the style we're using
-  const mapTilerUrl = 'https://osm2vectortiles.tileserver.com/v2.json';
-  const response = await fetch(mapTilerUrl, {
-                     mode: 'no-cors'
-                   }).catch(error => ({ok: false, error: error}));
-
-  if (!response.ok) {
-    console.debug('bad response from maptiler:', response);
-    return false;
-  }
-
-  // "cast to bool" just so we always return a true/false value
-  return await!!response.json().catch(function(error) {
-    console.debug(`could not fetch map tiler json: ${error}`);
-    return false;
-  });
-}
-
 function getAvailableTileLayers() {
   // Regular OSM server
   const osm = {
@@ -68,43 +25,26 @@ function getAvailableTileLayers() {
   });
 }
 
-// Only available vector layer is mapboxGL. Try validating we have webGL and we
-// can fetch the style file before we return it.
-// We also save the first backup tile layer so we can replace ourselves in the
-// map if we hit an error.
-function getAvailableVectorLayers(layersControl, tileLayers) {
+// Only available vector layer type is mapboxGL. Try validating we have webGL
+// before we use it.
+function getAvailableVectorLayers() {
   if (!isWebGLAvailable() || (false && !canFetchMapTiler()))
     return [];
 
-  let gl = null;
-  // Depending on availability of WebGL, we might be able to use vector tiles.
-  try {
-    // Add OSM vector maps, display using mapboxGL
-    gl = L.mapboxGL({
-      style:
-          'https://raw.githubusercontent.com/osm2vectortiles/mapbox-gl-styles/master/styles/bright-v9-cdn.json',
-      accessToken: 'no-token'
-    });
-    gl.on('add', function(event) {
-      // Sorry... reaching into the object as I can't find another way
-      const map = event.target._map;
-      gl.getMapboxMap().on('error', function(error) {
-        console.error('mapbox', error);
-        layersControl.removeLayer(gl);
-        if (map.hasLayer(gl)) {
-          console.debug('active layer was mapbox, replacing');
-          gl.remove();
-          // Assume there's at least one tile layer
-          map.addLayer(tileLayers[0].layer);
-          console.debug('replaced');
-        }
-      });
-    });
-  } catch (error) {
-    console.error('internal error creating mapbox gl layer', error);
-    return [];
-  }
-  return [{layer: gl, name: 'MapboxGL'}];
+  // Add UK Ordnance Survey maps, display using mapboxGL
+  const style_names = ['outdoor', 'road', 'light', 'night'];
+  return style_names.map(function(style) {
+    return {
+      name: `UK OS ${style}`,
+      layer: L.mapboxGL({
+        accessToken: 'no-token',
+        style:
+            `https://s3-eu-west-1.amazonaws.com/tiles.os.uk/v2/styles/open-zoomstack-${style}/style.json`,
+        attribution:
+            'Contains OS data &copy; Crown copyright and database rights 2018',
+      })
+    };
+  });
 }
 
 function createAndAttachMap(divId) {
@@ -114,21 +54,19 @@ function createAndAttachMap(divId) {
     zoom: 13,
   });
 
-  const layersControl = L.control.layers().addTo(map);
 
-  // We might need to remove vector layers later (on errors), so let's give it a
-  // reference to the layersControl.
   const tileLayers = getAvailableTileLayers();
-  const vectorLayers = getAvailableVectorLayers(layersControl, tileLayers);
+  const vectorLayers = getAvailableVectorLayers();
 
   const allLayers = vectorLayers.concat(tileLayers);
-
-  // Only add a layer to the map here, so we can add a vector one if we have one
+  // Add the best layer (vectors have priority) to the map
   allLayers[0].layer.addTo(map);
 
-  allLayers.forEach(
-      layerDef => layersControl.addBaseLayer(layerDef.layer, layerDef.name));
+  const layersControl = L.control.layers();
+  allLayers.forEach(obj => layersControl.addBaseLayer(obj.layer, obj.name));
 
+  // Add misc controls to map
+  layersControl.addTo(map);
   L.control.scale().addTo(map);
   return map;
 }

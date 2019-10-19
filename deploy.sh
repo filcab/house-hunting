e679413f1392161ad6b83faf58b7dfad1395dfbc
Filipe Cabecinhas <github@filcab.net>
2019-10-19 23:37:24 +0100

    Add controls.js to the deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 # Assumes destination directory already exist
 # Don't set group or owner, let the destination handle it
 # Don't set dir times, it was failing and I don't care
-rsync -avz --no-g --no-o -O apple-touch-icon.png index.html {leaflet,utils,map,popup,prefs,script}.js style.css dynamic *.json "$1"
+rsync -avz --no-g --no-o -O apple-touch-icon.png index.html {leaflet,utils,map,controls,popup,prefs,script}.js style.css dynamic *.json "$1"
